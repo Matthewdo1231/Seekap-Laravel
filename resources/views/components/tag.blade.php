0165f5b1f787444592d5338bcf8284c25f7861b6
MatthewDo <matthewdoarda@gmail.com>
2024-08-10 03:26:13 +0800

    implemented tags using alpines js states

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,32 +1,70 @@
+<!--
+
+    Complex Implementation, states from alpines js when clicking Tags and manually redirecting request using javascript url 
+
+-->
+
+
+
+
 @php
+  $jobTypes = ["Fulltime", "Part-Time"];
   $locations = ["Cebu", "Manila", "Davao"];
+  $companies = ["Jenkins-Parisian", "Hintz-Mueller", "Konopelski-Durgan", "Sanford-Hammes", "Ziemann-Senger"];
  @endphp
-
-<div class="flex flex-cols gap-10 mt-4 justify-center lg:justify-start md:ml-4 xl:w-[72rem] xl:mx-auto"> 
-
-  <div class="
-      text-sm text-gray-600 py-2 px-10 rounded-md bg-light-gray
+                   {{--Dynamic tags toggling--}}
+<div x-data="{ IsActive: null, isVisible:false }" class="flex flex-cols gap-10 mt-4 justify-center lg:justify-start md:ml-4 xl:w-[72rem] xl:mx-auto"> 
+                  
+  <div  @click = "
+    IsActive = (IsActive === 'tagJobType') ? null : 'tagJobType';
+    isVisible = (IsActive === 'tagJobType');"
+      class="
+      relative text-sm text-gray-600 py-2 px-10 rounded-md bg-light-gray
       lg:p-3">Job Type &#x25BE
-      @foreach($locations as $location)<div></div>@endforeach
+       <div x-cloak x-show="(IsActive == 'tagJobType' && isVisible)" class="absolute flex flex-col border-2 mt-3 top-10 left-0 z-10">
+        @foreach($jobTypes as $jobType)
+          <div class="py-2 px-10 whitespace-nowrap bg-white hover:bg-sky-200 hover:cursor-pointer">{{$jobType}}</div>
+          @endforeach
+        </div>
     </div>
-  <div class="
-  text-sm text-gray-600 py-2 px-10 rounded-md bg-light-gray
-   lg:p-3">Location &#x25BE
-    @foreach($locations as $location)<div></div>@endforeach
-   </div>
-  <div class="
-  text-sm text-gray-600 py-2 px-10 rounded-md bg-light-gray
-   lg:p-3">Company &#x25BE
-    @foreach($locations as $location)<div></div>@endforeach
-  </div>
+
+    <div  @click = "
+    IsActive = (IsActive === 'tagLocation') ? null : 'tagLocation';
+    isVisible = (IsActive === 'tagLocation');"
+      class="
+      relative text-sm text-gray-600 py-2 px-10 rounded-md bg-light-gray
+      lg:p-3">Location &#x25BE
+       <div x-cloak x-show="(IsActive == 'tagLocation' && isVisible) " class="absolute flex flex-col border-2 mt-3 top-10 left-0 z-10">
+        @foreach($locations as $location)
+          <div class="py-2 px-10 bg-white whitespace-nowrap hover:bg-sky-200 hover:cursor-pointer">{{$location}}</div>
+          @endforeach
+        </div>
+    </div>
+
+    <div  @click = "
+    IsActive = (IsActive === 'tagCompany') ? null : 'tagCompany';
+    isVisible = (IsActive === 'tagCompany');"
+      class="
+      relative text-sm text-gray-600 py-2 px-10 rounded-md bg-light-gray
+      lg:p-3">Company &#x25BE
+       <div x-cloak x-show="(IsActive == 'tagCompany' && isVisible) " class="absolute flex flex-col border-2 mt-3 top-10 left-0 z-10">
+        @foreach($companies as $company)
+          <div class="py-2 px-10 bg-white whitespace-nowrap hover:bg-sky-200 hover:cursor-pointer">{{$company}}</div>
+          @endforeach
+        </div>
+    </div>
 
 </div>
 
 <script>
+
+   
   
      /** document.getElementById('address-js').addEventListener('click', function(event) {
       let url = new URL(window.location.href);
       url.searchParams.set('tag', 'AK');
       window.location.href = url;
   });**/
+
 </script>
+
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,8 +1,8 @@
 <x-layout>
   <x-navbar/>
   <x-searchbar/>
-  <x-tag/>
   @if(!empty($joblistings))
+  <x-tag/>
   <x-joblistings :joblistings="$joblistings" :joblistingFullDesc="$joblistingFullDesc"/>
     <div class="mt-6 p-4">
       {{$joblistings->links('vendor.pagination.tailwind')}}
